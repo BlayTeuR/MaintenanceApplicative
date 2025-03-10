b3940f28d8ec8b1194b720f7792c196d103f43ee
Bastien Jallais <bastien.jallais9@etu.univ-lorraine.fr>
2025-03-10 14:28:04 +0100

    [DEV] handle fini

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,7 @@ class GildedRose {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,54 +17,72 @@ class GildedRose {
         }
     }
 
-    public void handle(Item item) {
-        if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-            if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                    item.quality = item.quality - 1;
-                }
-            }
+    private void handle(Item item) {
+        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            handleSulfuras(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            handleBackstage(item);
+        } else if (item.name.equals(AGED_BRIE)) {
+            handleAgedBrie(item);
+        } else if (item.name.toLowerCase().contains("conjured")) {
+            handleConjured(item);
         } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            handleNormal(item);
+        }
+    }
 
-                if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+    private void handleNormal(Item item) {
+        item.sellIn--;
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality--;
+        }
+    }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
+    private void handleConjured(Item item) {
+        item.sellIn--;
+        if (item.quality > 0) {
+            item.quality -= 2;
+            if (item.quality < 0) {
+                item.quality = 0;
             }
         }
-
-        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality -= 2;
+            if (item.quality < 0) {
+                item.quality = 0;
+            }
         }
+    }
+
+    private void handleAgedBrie(Item item) {
+        item.sellIn--;
+
+        if (item.quality < 50) {
+            item.quality++;
+        }
+
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void handleBackstage(Item item) {
+        item.sellIn--;
 
         if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+            item.quality = Math.min(50, item.quality + 3);
+        } else if (item.sellIn < 10) {
+            item.quality = Math.min(50, item.quality + 2);
+        } else {
+            item.quality = Math.min(50, item.quality + 1);
         }
+    }
+
+    private void handleSulfuras(Item item) {
     }
 }
